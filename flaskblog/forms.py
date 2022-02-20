c17f13a98762a707a800ff7852e2c480e15538ce
yahia <yahiakhairy@gmail.com>
2022-02-20 19:17:35 +0000

    new

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,5 +1,7 @@
+from flask_login import current_user
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_wtf.file import FileField, FileAllowed
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
 from wtforms.validators import DataRequired, Length, EqualTo, Email, ValidationError
 from flaskblog.models import User
 
@@ -27,3 +29,28 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first() #does a query like "... WHERE username = username.data" to check if we get a result or not
+            if user: #if there are no results returned then user is null value (False), but if it returns a value it must mean another user with that username exists
+                raise ValidationError('That username is taken. Please choose a different one')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first() #does a query like "... WHERE username = username.data" to check if we get a result or not
+            
+            if user: #if there are no results returned then user is null value (False), but if it returns a value it must mean another user with that username exists
+                raise ValidationError('That email is taken. Please choose a different one')
+
+
+class PostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    content = TextAreaField('Content', validators=[DataRequired()])
+    submit = SubmitField('Post')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,28 +1,20 @@
-from flask import Flask, render_template, url_for, flash, redirect, request
+from flask import Flask, render_template, url_for, flash, redirect, request, abort
 from flaskblog import app, db, bcrypt
-from flaskblog.forms import RegistrationForm, LoginForm
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from flaskblog.models import User, Post
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+from PIL import Image
+import os
 
-posts = [
-    {
-        'author': 'Yahia Abdelkader',
-        'title': 'Blog Post 1',
-        'content': 'www.youtube.com',
-        'date_posted': 'April 20, 2018'
-    },
-      {
-        'author': 'Another User',
-        'title': 'Blog Post 2',
-        'content': 'second post content',
-        'date_posted': 'April 21, 2018'
-    },
-]
+
 
 @app.route("/")
 @app.route("/home")
 @login_required
 def home():
+    page = request.args.get('page', 1, type=int)
+    posts = Post.query.order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
@@ -58,10 +50,17 @@ def login():
             login_user(user, remember=form.remember.data)
             flash(f'Welcome back {user.username}!', 'success')
             next_page = request.args.get('next')
-            if next_page[1:] == 'home':
-                return redirect(url_for('home'))
-            elif next_page[1:] == 'account':
-                return redirect(url_for('account'))
+            
+            if next_page:
+                if len(next_page) > 1:
+                    if next_page[1:] == 'home':
+                        return redirect(url_for('home'))
+                    elif next_page[1:] == 'account':
+                        return redirect(url_for('account'))
+                else:
+                    return redirect(url_for('home'))
+            else:
+                 return redirect(url_for('home'))
             
         else:
             flash('Login Unsuccessful. Please check email and password', 'danger')
@@ -72,7 +71,88 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    output_size = (125, 125)
+    i = Image.open(form_picture) # Image allows us to save a picture as an object to use methods to modify aspects of picture through code
+    i.thumbnail(output_size)    #thumbnail and output size ensures every image is sized to a standardized small sized before saving for faster data transfer and to take up less storage on file system
+    i.save(picture_path)    #save function saves file onto path passed as an argument
+    return picture_fn
+
+@app.route('/account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title='Account')
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
+
+@login_required
+@app.route('/post/new', methods=['GET', 'POST'])  
+def new_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Your post has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_post.html', title='New Post', form=form, legend='Create Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
+
+@login_required
+@app.route("/post/<int:post_id>/update", methods=['GET', 'POST'])
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Your post has been updated!', 'success')
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+    return render_template('create_post.html', title='New Post', form=form, legend='Update Post')
+
+@login_required
+@app.route("/post/<int:post_id>/delete", methods=['POST'])
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Your post has been deleted!', 'success')
+    return redirect(url_for('home'))
+
+
+@app.route("/user/<string:username>")
+@login_required
+def user_posts(username):
+    page = request.args.get('page', 1, type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(per_page=5, page=page)
+    return render_template('user_posts.html', posts=posts, user=user)
